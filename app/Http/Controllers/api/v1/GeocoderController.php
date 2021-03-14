6a506c6e1adb8229a53ec42b94f3d1c7ded458a0
Vasily Nazarov <vnaz@mail.ru>
2021-03-14 17:28:10 +0400

    step1

--- app/Http/Controllers/api/v1/GeocoderController.php
+++ app/Http/Controllers/api/v1/GeocoderController.php
@@ -10,74 +10,77 @@ use Yandex\Geocode\Facades\YandexGeocodeFacade;
 
 class GeocoderController extends Controller
 {
-    public function search(Request $request){
+    public function search(Request $request)
+    {
 
-        $validate = Validator::make($request->all(),[
-            'apikey'=>'required',
-            'address'=>'string',
-            'city'=>'string',
-            'street'=>'string',
-            'building'=>'string',
-            'flat'=>'string',
+        $validate = Validator::make($request->all(), [
+            'apikey' => 'required',
+            'address' => 'string',
+            'city' => 'string',
+            'street' => 'string',
+            'building' => 'string',
+            'flat' => 'string',
         ]);
-        if($validate->fails()){
-            return ['error'=>'401','message'=>$validate->getMessageBag()];
+        if ($validate->fails()) {
+            return ['error' => '401', 'message' => $validate->getMessageBag()];
         }
-        if(!\App\Models\ApiKey::checkApiKey($request->input('apikey'))){
-            return ['error'=>'408','message'=>'The Api Key doesn\'t exist or you don\'t have access to it'];
+        if (!\App\Models\ApiKey::checkApiKey($request->input('apikey'))) {
+            return ['error' => '408', 'message' => 'The Api Key doesn\'t exist or you don\'t have access to it'];
         }
-        if($request->input('address')) {
+        if ($request->input('address')) {
             $address_field = $request->input('address');
 
-        }else{
+        } else {
             $address_field = '' . $request->input('city') . ' ' . $request->input('street')
                 . ' ' . $request->input('building') . ' ' . $request->input('flat');
         }
 
         $cash = CacheGeocode::checkCash($address_field);
-        if($cash){
+        if ($cash) {
             $cash = ($cash);
-        }else{
-            $data = YandexGeocodeFacade::setQuery('  '.$address_field)->load();
+        } else {
+            $data = YandexGeocodeFacade::setQuery('  ' . $address_field)->load();
             $cash = $data->getResponse()->getRawData();
             $cash = (array)($cash);
-            CacheGeocode::setCash($address_field,$cash);
+            CacheGeocode::setCash($address_field, $cash);
         }
-
 
 
 //        return ($cash['metaDataProperty']['GeocoderMetaData']['precision']);
 
         $match = true;
-        if($cash['metaDataProperty']['GeocoderMetaData']['precision'] !='exact')
+        if ($cash['metaDataProperty']['GeocoderMetaData']['precision'] != 'exact')
             $match = false;
         $data = [];
         $data['match'] = $match;
-        $data['result']['precision']=$cash['metaDataProperty']['GeocoderMetaData']['precision'];
-        $data['result']['text']= $cash['metaDataProperty']['GeocoderMetaData']['Address']['formatted'];
-        $data['result']['kind']= $cash['metaDataProperty']['GeocoderMetaData']['kind'];
+        $data['result']['precision'] = $cash['metaDataProperty']['GeocoderMetaData']['precision'];
+        $data['result']['text'] = $cash['metaDataProperty']['GeocoderMetaData']['Address']['formatted'];
+        $data['result']['kind'] = $cash['metaDataProperty']['GeocoderMetaData']['kind'];
 
-        return response()->json($data,200,['Content-type'=>'application/json;charset=utf-8'],JSON_UNESCAPED_UNICODE);
+        return response()->json($data, 200, ['Content-type' => 'application/json;charset=utf-8'], JSON_UNESCAPED_UNICODE);
     }
 
-    public function locality(Request $request){
-        $validate = Validator::make($request->all(),[
-            'apikey'=>'required',
-            'city'=>'string|required',
-            'lang'=>'string|min:2|max:2',
+    public function locality(Request $request)
+    {
+        echo __METHOD__;die;
+
+        $validate = Validator::make($request->all(), [
+            'apikey' => 'required',
+            'city' => 'string|required',
+            'lang' => 'string|min:2|max:2',
 
 
         ]);
 
-        if($validate->fails()){
-            return ['error'=>'401','message'=>$validate->getMessageBag()];
+        if ($validate->fails()) {
+            return ['error' => '401', 'message' => $validate->getMessageBag()];
         }
 
-        if(!\App\Models\ApiKey::checkApiKey($request->input('apikey'))){
-            return ['error'=>'408','message'=>'The Api Key doesn\'t exist or you don\'t have access to it'];
+        if (!\App\Models\ApiKey::checkApiKey($request->input('apikey'))) {
+            return ['error' => '408', 'message' => 'The Api Key doesn\'t exist or you don\'t have access to it'];
         }
 
-        $url = 'http://geohelper.info/api/v1/cities?apiKey='.env('GEOHELPER_API_KEY').'&filter[name]='.$request->input('city').'&locale[lang]='.$request->input('lang');
+        $url = 'http://geohelper.info/api/v1/cities?apiKey=' . env('GEOHELPER_API_KEY') . '&filter[name]=' . $request->input('city') . '&locale[lang]=' . $request->input('lang');
 
         $response = \Illuminate\Support\Facades\Http::withOptions([
 //                        'debug' => true,
@@ -88,75 +91,81 @@ class GeocoderController extends Controller
 
         $response = json_decode($response->body());
         $rez = $response->result;
-        foreach ($rez as $item){
+        foreach ($rez as $item) {
             $returnA[count($returnA)]['id'] = $item->id;
-            $returnA[count($returnA)-1]['name'] ='';
-            if(isset($item->localityType->name))
-                $returnA[count($returnA)-1]['name'] = $item->localityType->name.' ';
-            $returnA[count($returnA)-1]['name'] .= $item->name;
-            if(isset($item->area ) ) {
-                $returnA[count($returnA)-1]['name'] .= ', '.$item->area.' район' ;
+            $returnA[count($returnA) - 1]['name'] = '';
+            if (isset($item->localityType->name))
+                $returnA[count($returnA) - 1]['name'] = $item->localityType->name . ' ';
+            $returnA[count($returnA) - 1]['name'] .= $item->name;
+            if (isset($item->area)) {
+                $returnA[count($returnA) - 1]['name'] .= ', ' . $item->area . ' район';
             }
-
 
 
 //            else{
-                $url = 'http://geohelper.info/api/v1/regions?apiKey='.env('GEOHELPER_API_KEY').'&filter[id]='.$item->regionId.
-                    '&locale[lang]='.$request->input('lang');
+            $url = 'http://geohelper.info/api/v1/regions?apiKey=' . env('GEOHELPER_API_KEY') . '&filter[id]=' . $item->regionId .
+                '&locale[lang]=' . $request->input('lang');
 
-                $response = \Illuminate\Support\Facades\Http::withOptions([
+            $response = \Illuminate\Support\Facades\Http::withOptions([
 //                        'debug' => true,
-                ])->get($url);
-                $response = json_decode($response->body());
-                $response = $response->result[0];
+            ])->get($url);
+            $response = json_decode($response->body());
+            $response = $response->result[0];
 
 //                $returnA[count($returnA)-1]['regionId'] =$response->name;
-                $returnA[count($returnA)-1]['name'] .= ', '.$response->name;
-            }
+            $returnA[count($returnA) - 1]['name'] .= ', ' . $response->name;
+        }
 
-        return ["success"=> true, "language"=> $request->input('lang'),'result'=>$returnA];
+        return ["success" => true, "language" => $request->input('lang'), 'result' => $returnA];
 
     }
 
-    public function street(Request $request){
-        $validate = Validator::make($request->all(),[
-            'apikey'=>'required',
-            'city'=>'int|required',
-            'street'=>'string|required',
-            'lang'=>'string|min:2|max:2',
+    public function street(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'apikey' => 'required',
+            'city' => 'int|required',
+            'street' => 'string|required',
+            'lang' => 'string|min:2|max:2',
 
 
         ]);
 
-        if($validate->fails()){
-            return ['error'=>'401','message'=>$validate->getMessageBag()];
+        if ($validate->fails()) {
+            return ['error' => '401', 'message' => $validate->getMessageBag()];
         }
 
-        if(!\App\Models\ApiKey::checkApiKey($request->input('apikey'))){
-            return ['error'=>'408','message'=>'The Api Key doesn\'t exist or you don\'t have access to it'];
+        if (!\App\Models\ApiKey::checkApiKey($request->input('apikey'))) {
+            return ['error' => '408', 'message' => 'The Api Key doesn\'t exist or you don\'t have access to it'];
         }
 
-        $url = 'http://geohelper.info/api/v1/streets?apiKey='.env('GEOHELPER_API_KEY').'&filter[name]='.$request->input('street').
-            '&locale[lang]='.$request->input('lang').'&filter[cityId]='.$request->input('city');
+        $url = 'http://geohelper.info/api/v1/streets?apiKey=' . env('GEOHELPER_API_KEY') . '&filter[name]=' . $request->input('street') .
+            '&locale[lang]=' . $request->input('lang') . '&filter[cityId]=' . $request->input('city');
 
         $response = \Illuminate\Support\Facades\Http::withOptions([
 //                        'debug' => true,
         ])->get($url);
-        $returnA = [];
 
-        $response = json_decode($response->body());
-        $rez = $response->result;
-        foreach ($rez as $item){
-            $returnA[count($returnA)]['id'] = $item->id;
-            $returnA[count($returnA)-1]['name'] = '';
-            if(isset($item->localityType->name))
-                $returnA[count($returnA)-1]['name'] .= $item->localityType->name.' ';
-            $returnA[count($returnA)-1]['name'] .= $item->name;
-            $returnA[count($returnA)-1]['cityId'] = $item->cityId;
-//            $returnA[count($returnA)-1]['postCode'] = $item->postCode;
+        $data = json_decode($response->body(), true)['result'];
+        $data = json_decode($response->body())->result;
+
+        $result = [];
+//var_dump($data);;die;
+        foreach ($data as $item) {
+//!!!!!            $item = (object)$item;
+            $resultItem['id'] = $item->id;
+            $name = '';
+            if (isset($item->localityType->name))
+                $name .= $item->localityType->name . ' ';
+            $name .= $item->name;
+            $resultItem['name'] = $name;
+            $resultItem['cityId'] = $item->cityId;
+//?            $resultItem['postCode'] = $item->postCode;
+
+            $result[] = $resultItem;
         }
 
-        return ["success"=> true, "language"=> $request->input('lang'),'result'=>$returnA];
+        return ["success" => true, "language" => $request->input('lang'), 'result' => $result];
 
     }
 
--- app/Services/CacheGeocode.php
+++ app/Services/CacheGeocode.php
@@ -6,17 +6,23 @@ namespace App\Services;
 
 class CacheGeocode
 {
-    public static function checkCash($address){
-        $cash = \App\Models\CacheGeocode::where('request',$address)->first();
-        if($cash){
+    public static function checkCash($address)
+    {
+
+        return null; // #x21-01 no cache
+
+        $cash = \App\Models\CacheGeocode::where('request', $address)->first();
+        if ($cash) {
             return $cash->result;
-        }
-        else
+        } else
             return null;
     }
 
-    public static function setCash($address,$result){
-        $cash  = new \App\Models\CacheGeocode();
+    public static function setCash($address, $result)
+    {
+        return; // #x21-01 no cache
+
+        $cash = new \App\Models\CacheGeocode();
 
         $cash->request = $address;
         $cash->result = $result;
